793ab7e1a4e185148722ce6a07ee57a7d1c89924
Ariel Halili <arielhalili259@gmail.com>
2023-07-26 15:11:04 +0300

    continue

--- AHClawBot/AHClawBot.py
+++ AHClawBot/AHClawBot.py
@@ -22,13 +22,16 @@ with open('C:/AHClawBot-token.dat') as f:
 def get_help_message():
 	return '''Bot about the awesome game Claw
 You can use the following commands:
+/claw - get info about Captain Claw
+/story - get the story of the game
 /level <level number> - get info about level
-/claw - get info about Claw
 /enemies - get list of enemies
-/enemy <enemy name> - get info about enemy
 /bosses - get list of bosses
-/boss <boss name> - get info about boss
 /help - get help (this message)
+
+You can also use the following commands:
+enemy <enemy name> - get info about enemy
+boss <boss name> - get info about boss
 
 if you want you can see the recluse website:
 https://captainclaw.net/en/index.html
@@ -46,46 +49,47 @@ def level(message: telebot.types.Message):
 		bot.send_message(message.chat.id, 'the format of the command is "level <level number>"')
 	else:
 		level_number = splited_text[1]
-		bot.send_message(message.chat.id, ccdata.get_level_data(level_number))
+		bot.send_message(message.chat.id, ccdata.get_level_data(level_number), parse_mode='Markdown')
 
 
 @bot.message_handler(commands=['claw'])
 def claw(message: telebot.types.Message):
-	bot.send_message(message.chat.id, 'you choosed claw')
+	bot.send_sticker(message.chat.id, sticker=ccdata.get_sticker('claw'))
+	bot.send_message(message.chat.id, 'Captain Claw is the main character of this game.\nSee the story for more information.')
+	
+@bot.message_handler(commands=['story'])
+def story(message: telebot.types.Message):
+	bot.send_message(message.chat.id, ccdata.story)
 
 
 @bot.message_handler(commands=['enemies'])
 def enemies(message: telebot.types.Message):
-	bot.send_message(message.chat.id, '\n'.join(ccdata.get_enemies_list()))
+	bot.send_message(message.chat.id, '\n'.join(ccdata.enemies_list))
 
 
 @bot.message_handler(commands=['enemy'])
 def enemy(message: telebot.types.Message):
 	splited_text =  message.text.split()
 	enemy_name = ' '.join(splited_text[1:])
-	bot.send_message(message.chat.id, ccdata.get_enemy_data(enemy_name))
 
-	sticker = ccdata.get_enemy_sticker(enemy_name)
+	sticker = ccdata.get_sticker(enemy_name)
 	if isinstance(sticker, tuple):
 		for i in sticker:
 			bot.send_sticker(message.chat.id, sticker=i)
 	elif sticker is not None:
 		bot.send_sticker(message.chat.id, sticker=sticker)
 
+	bot.send_message(message.chat.id, ccdata.get_enemy_data(enemy_name))
+
 
 @bot.message_handler(commands=['bosses'])
 def bosses(message: telebot.types.Message):
-	bot.send_message(message.chat.id, '\n'.join(ccdata.get_bosses_list()))
+	bot.send_message(message.chat.id, '\n'.join(ccdata.bosses_list))
 
 
 @bot.message_handler(commands=['boss'])
 def boss(message: telebot.types.Message):
-	splited_text =  message.text.split()
-	boss_name = ' '.join(splited_text[1:])
-	bot.send_message(message.chat.id, ccdata.get_boss_data(boss_name))
-	sticker = ccdata.get_enemy_sticker(boss_name)
-	if sticker is not None:
-		bot.send_sticker(message.chat.id, sticker=sticker)
+	enemy(message)
 
 
 @bot.message_handler(commands=['help'])
@@ -97,11 +101,14 @@ def help(message: telebot.types.Message):
 def chatting(message: telebot.types.Message):
 	cmd = message.text.split()[0].lower()
 	if cmd == 'level': level(message)
+	elif cmd == 'story': story(message)
+	elif cmd == 'claw': claw(message)
 	elif cmd == 'enemy': enemy(message)
 	elif cmd == 'boss': boss(message)
 	else: bot.send_message(message.chat.id, 'unknown command')
 
 
 if __name__ == '__main__':
+	ccdata.init()
 	print('bot is running')
 	bot.infinity_polling(skip_pending=True)
--- AHClawBot/ccdata.py
+++ AHClawBot/ccdata.py
@@ -1,17 +1,26 @@
 import json
 
 
-with open('./data/levels_data.json') as f: levels_data = json.load(f)
-with open('./data/enemies_data.json') as f: enemies_data = json.load(f)
-with open('./data/bosses_data.json') as f: bosses_data = json.load(f)
+levels_data = None
+enemies_data = None
+enemies_list = None
+bosses_list = None
+story = None
 
 
-def get_enemies_list():
-	return [i for i in enemies_data.keys()]
+def init():
+	global levels_data, enemies_data, enemies_list, bosses_list, story
 
-
-def get_bosses_list():
-	return [i for i in bosses_data.keys()]
+	# TODO: all data should be in one file
+	with open('./data/levels_data.json') as f: levels_data = json.load(f)
+	with open('./data/enemies_data.json') as f: enemies_data = json.load(f)
+	with open('./data/bosses_data.json') as f: bosses_data = json.load(f)
+	with open('./data/story.txt') as f: story = f.read()
+	
+	enemies_list = [i for i in enemies_data.keys()]
+	bosses_list = [i for i in bosses_data.keys()]
+	
+	enemies_data = dict((k.lower(), v) for k, v in { **enemies_data, **bosses_data }.items())
 
 
 def get_level_data(n):
@@ -21,28 +30,25 @@ def get_level_data(n):
 
 
 def get_enemy_data(enemy_name):
+	enemy_name = enemy_name.lower()
 	if enemy_name in enemies_data.keys():
 		return '\n'.join(enemies_data[enemy_name])
 	return 'invalid enemy'
 
 
-def get_boss_data(boss_name):
-	if boss_name in bosses_data.keys():
-		return '\n'.join(bosses_data[boss_name])
-	return 'invalid enemy'
+def get_sticker(name):
+	name = name.lower()
 
-
-def get_enemy_sticker(enemy_name):
-	if enemy_name in enemies_data.keys():
-		if enemy_name == 'Town Guard':
+	if name in enemies_data.keys():
+		if name == 'town guard':
 			return (open('data/images/town guard 1.png', 'rb'), open('data/images/town guard 2.png', 'rb'))
 		
-		elif enemy_name == 'Tiger Guard':
+		elif name == 'tiger guard':
 			return (open('data/images/tiger guard.png', 'rb'), open('data/images/tiger guard white.png', 'rb'))
 		
-		return open(f'data/images/{enemy_name}.png', 'rb')
+		return open(f'data/images/{name}.png', 'rb')
 	
-	elif enemy_name in bosses_data.keys():
-		return open(f'data/images/{enemy_name}.png', 'rb')
+	elif name == 'claw':
+		return open(f'data/images/{name}.png', 'rb')
 	
 	return None
